af51b2b53a8713ae3a65d02c06dffa406aea3833
Karan Patel <96780293+karanop001018@users.noreply.github.com>
2024-04-30 01:32:07 +0530

    Add files via upload

---
+++ practical_32.java
@@ -0,0 +1,18 @@
+class ArgumentGreaterThanOne extends Exception {
+    ArgumentGreaterThanOne(String message) {
+        super(message);
+    }
+}
+public class practical_32 {
+    public static void main(String[] args) throws ArgumentGreaterThanOne {
+        try {
+            if (args.length> 1) {
+                throw new ArgumentGreaterThanOne("More than one argument provided.");
+            } else {
+                System.out.println("Successfully ran without Exception.");
+            }
+        } catch (ArgumentGreaterThanOne e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
